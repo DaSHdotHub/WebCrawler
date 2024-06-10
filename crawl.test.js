6ad76c23a02686a767ff870f7dd3f4479ad930fc
Daniil Shaulov <dshaulov@outlook.com>
2024-06-11 00:40:22 +0200

    getUrlFromHTML function and tests

--- crawl.test.js
+++ crawl.test.js
@@ -1,4 +1,4 @@
-const {normalizeURL} = require('./crawl.js')
+const {normalizeURL, getUrlFromHTML} = require('./crawl.js')
 const {test, expect} = require('@jest/globals')
 
 test('normalizeURL strip https protocol', () => {
@@ -28,3 +28,73 @@ test('normalizeURL strp http protocol', () => {
     const expected = 'github.com/dashdothub'
     expect(actual).toEqual(expected)
     })
+
+test('getUrlFromHTML absolute', () => {
+    const inputHTMLBody = `
+    <html>
+        <body>
+            <a href="https://github.com/profile/">
+                GitHub
+            </a>
+        </body>
+    </html>
+    `
+    const inputBaseUrl = "https://github.com"
+    const actual = getUrlFromHTML(inputBaseUrl, inputHTMLBody)
+    const expected = ["https://github.com/profile/"]
+    expect(actual).toEqual(expected)
+    })
+
+test('getUrlFromHTML relative', () => {
+    const inputHTMLBody = `
+    <html>
+        <body>
+            <a href="/path/">
+                GitHub
+            </a>
+        </body>
+    </html>
+    `
+    const inputBaseUrl = "https://github.com"
+    const actual = getUrlFromHTML(inputBaseUrl, inputHTMLBody)
+    const expected = ["https://github.com/path/"]
+    expect(actual).toEqual(expected)
+    })
+
+test('getUrlFromHTML combination', () => {
+    const inputHTMLBody = `
+    <html>
+        <body>
+            <a href="/path/">
+                GitHub
+            </a>
+            <a href="https://gitlab.com/"
+                Gitlab
+            </a>
+        </body>
+    </html>
+    `
+    const inputBaseUrl = "https://github.com"
+    const actual = getUrlFromHTML(inputBaseUrl, inputHTMLBody)
+    const expected = ["https://github.com/path/", "https://gitlab.com/"]
+    expect(actual).toEqual(expected)
+    })
+
+    test('getUrlFromHTML invalid', () => {
+        const inputHTMLBody = `
+        <html>
+            <body>
+                <a href="invalid">
+                    Invalid URL
+                </a>
+                <a href="https://gitlab.com/"
+                    Gitlab
+                </a>
+            </body>
+        </html>
+        `
+        const inputBaseUrl = "https://github.com"
+        const actual = getUrlFromHTML(inputBaseUrl, inputHTMLBody)
+        const expected = ["https://gitlab.com/"]
+        expect(actual).toEqual(expected)
+        })
